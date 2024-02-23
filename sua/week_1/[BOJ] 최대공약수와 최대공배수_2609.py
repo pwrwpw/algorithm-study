fa8bd23e4135e2964b8934f1218e31e14f04e6b0
soapcookie <sgua0720@gmail.com>
2024-02-23 19:58:00 +0900

    add 2609

---
+++ sua/week_1/[BOJ] 최대공약수와 최대공배수_2609.py
@@ -0,0 +1,24 @@
+#내장함수 사용
+import math
+
+a,b = map(int,input().split())
+
+print(math.gcd(a,b))
+print(math.lcm(a,b))
+
+#유클리드 호제법
+a,b = map(int,input().split())
+
+# 최대공약수
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+# 최소공배수 
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+print(gcd(a, b))
+print(lcm(a, b))
