4297e474c8e8588cb02eea3d4d005801455e6cbc
kimseungwoo <aoome123@naver.com>
2024-02-28 02:21:28 +0900

    [BOJ] 2609 / 브론즈 1

--- Kimseungwoo0407/math/최대공약수와 최소공배수.py
+++ Kimseungwoo0407/math/최대공약수와 최소공배수.py
@@ -3,6 +3,7 @@
 a,b = map(int,input().split())
 
 # 최대공약수
+
 # a & b의 최대 공약수는 b & a를 b로 나눈 나머지의 최대 공약수
 def gcd(a, b):
     if a < b:
@@ -12,6 +13,7 @@ def gcd(a, b):
     return a
 
 # 최소공배수
+
 # a와 b의 곱을 a와 b의 최대 공약수로 나눈 값
 def lcm(a, b):
     return a * b // gcd(a, b)
