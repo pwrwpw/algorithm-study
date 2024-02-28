015ce24637f60bcece5376b5fc2da1dd67a45280
pwrwpw <dlgustjr9912@gmail.com>
2024-02-28 21:12:26 +0900

    [BOJ] 2217 / 실버4

---
+++ pwrwpw/week_1/[BOJ]로프_2217.py
@@ -0,0 +1,15 @@
+# 시간 복잡도 : NlogN
+
+n = int(input())
+
+ropes = []
+
+for _ in range(n):
+    ropes.append(int(input()))
+
+ropes.sort(reverse=True)
+
+for i in range(n):
+    ropes[i] = ropes[i] * (i+1)
+
+print(max(ropes))
