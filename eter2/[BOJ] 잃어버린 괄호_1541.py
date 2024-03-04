09a7c0550f0688327064d5743afaf0a7ea1fdf14
eter2 <eter2hy@gmail.com>
2024-03-04 12:03:07 +0900

    [BOJ] 1541 / 실버2

---
+++ eter2/[BOJ] 잃어버린 괄호_1541.py
@@ -0,0 +1,16 @@
+e = input().split('-')
+
+num = []
+
+for i in e:
+    sum = 0
+    tmp = i.split('+')
+    for j in tmp:
+        sum += int(j)
+    num.append(sum)
+
+n = num[0]
+
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
