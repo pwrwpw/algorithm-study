868d1ff2437791b1c4beff459901dcbc2fb454e6
kimseungwoo <aoome123@naver.com>
2024-03-28 23:38:34 +0900

    [BOOK]

---
+++ Kimseungwoo0407/binary-search/[BOOK] 이진 탐색(재귀).py
@@ -0,0 +1,25 @@
+# 이진 탐색 소스코드 구현 (재귀 함수)
+def binary_search(array, target, start, end):
+    # 탐색할 범위가 없다
+    if start > end:
+        return None
+
+    mid = (start + end) // 2
+
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+
+n, target = list(map(int, input().split()))
+
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print('원소가 존재하지 않습니다.')
+else:
+    print(result + 1)
