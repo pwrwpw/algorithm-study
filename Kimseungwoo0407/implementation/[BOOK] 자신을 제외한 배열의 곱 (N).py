f08e12bb0431e402bed9809374297e1cef0e8bd0
kimseungwoo <aoome123@naver.com>
2024-04-03 23:17:03 +0900

    [BOOK]]

---
+++ Kimseungwoo0407/implementation/[BOOK] 자신을 제외한 배열의 곱 (N).py
@@ -0,0 +1,13 @@
+def product_self(nums):
+    out = []
+    p = 1
+
+    for i in range(0,len(nums)):
+        out.append(p)
+        p = p * nums[i]
+    p = 1
+
+    for i in range(len(nums)-1,0-1,-1):
+        out[i] = out[i] * p
+        p = p * nums[i]
+    return out
