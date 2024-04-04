e4e207f76bba67fa2c276f2ccb3f925572f091bf
kimseungwoo <aoome123@naver.com>
2024-04-05 00:37:24 +0900

    [BOJ] 2468 / 실버1

--- Kimseungwoo0407/dfs-bfs/[BOJ] 안전영역_2468.py
+++ Kimseungwoo0407/dfs-bfs/[BOJ] 안전영역_2468.py
@@ -1,48 +1,41 @@
 from collections import deque
 
-N = int(input())
-graph = []
-maxNum = 0
+def solve(h):
+    cnt = 0
+    for i in range(N):
+        for j in range(N):
+            if v[i][j] == 0 and arr[i][j] > h:
+                bfs(i,j,h)
+                cnt +=1
+    return cnt
 
-for i in range(N):
-    graph.append(list(map(int,input().split())))
-    for j in range(N):
-        if graph[i][j] > maxNum:
-            maxNum = graph[i][j]
+def bfs(x,y,h):
+    queue = deque()
+    queue.append((x,y))
+    v[x][y] = 1
+
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if 0<=nx<N and 0<=ny<N:
+                if v[nx][ny] == 0 and arr[nx][ny] > h:
+                    queue.append((nx,ny))
+                    v[nx][ny] = 1
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def bfs(x,y,value,visited):
+N = int(input())
 
-    queue = deque()
-    queue.append((x,y))
-    visited[x][y] = 1
+arr = [list(map(int,input().split())) for _ in range(N)]
 
-    while queue:
-        x,y = queue.popleft()
+ans = 0
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+for h in range(100):
+    v = [ [0] * N for _ in range(N)]
+    ans = max(ans,solve(h))
 
-            if 0 <= nx < N and 0 <= ny < N:
-                if graph[nx][ny] > value and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    queue.append((nx,ny))
-result = 0
-
-for i in range(maxNum):
-    visited = [ [0]*N for _ in range(N)]
-    cnt = 0
-
-    for j in range(N):
-        for k in range(N):
-            if graph[j][k] > i and visited[j][k] == 0:
-                bfs(j,k,i,visited)
-                cnt +=1
-
-    if result < cnt:
-        result = cnt
-
-print(result)
+print(ans)
