38f0ce96b6ca8987d91f05a4e58230c65323bf95
SmallGhost <69885380+j02j@users.noreply.github.com>
2024-04-08 19:38:03 +0900

    [ETC] - DP 기초

---
+++ JJY/week7/DP/fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    
+    return dp[n]
+
+# 예시 입력과 출력
+print(fibonacci(10))  # 55
+print(fibonacci(20))  # 6765
