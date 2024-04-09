834eaad0b0ed6421bbfdaacc1554168c8c7fba73
kimseungwoo <aoome123@naver.com>
2024-04-09 23:31:34 +0900

    [BOJ] 2667 / 실버1

--- Kimseungwoo0407/dfs-bfs/[BOJ] 단지번호붙이기_2667.py
+++ Kimseungwoo0407/dfs-bfs/[BOJ] 단지번호붙이기_2667.py
@@ -1,41 +1,33 @@
-n = int(input())
+N = int(input())
 
-graph = []
-
-for i in range(n):
-    graph.append(list(map(int,input())))
+graph = [ list(map(int,input())) for _ in range(N)]
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def dfs(x,y):
-    if x < 0 or x >= n or y < 0 or y>=n:
-        return False
 
+def dfs(x,y):
+    global count
     if graph[x][y] == 1:
-        global count
         count +=1
-        graph[x][y] =0
+        graph[x][y] = 0
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx,ny)
-        return True
-    return False
-
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 1:
+                dfs(nx,ny)
 count = 0
-result = 0
-nums = []
+result = []
 
-for i in range(n):
-    for j in range(n):
-        if dfs(i,j) == True:
-            nums.append(count)
-            result +=1
-            count = 0
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            dfs(i,j)
+            result.append(count)
+            count=0
 
-nums.sort()
-print(result)
-for i in nums:
+print(len(result))
+result.sort()
+for i in result:
     print(i)
