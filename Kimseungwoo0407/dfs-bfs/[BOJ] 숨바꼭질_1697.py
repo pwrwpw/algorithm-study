f4ec77f43da687a805310004c3153f2a750b44ad
kimseungwoo <aoome123@naver.com>
2024-04-11 22:42:24 +0900

    [BOJ] 1697 / 실버1

---
+++ Kimseungwoo0407/dfs-bfs/[BOJ] 숨바꼭질_1697.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+visited = [float('inf')] * 100001
+
+count = 0
+move = [-1, 1, 2]
+
+
+def bfs(N, K):
+    queue = deque()
+    queue.append(N)
+    visited[N] = 0
+    while queue:
+        x = queue.popleft()
+
+        if x == K:
+            return visited[x]
+
+        for i in move:
+            if i != 2:
+                nx = x + i
+            else:
+                nx = x * i
+
+            if 0 <= nx < 100001 and visited[nx] > visited[x] + 1:
+                visited[nx] = visited[x] + 1
+                queue.append(nx)
+
+    return visited[K]
+
+
+print(bfs(N, K))
